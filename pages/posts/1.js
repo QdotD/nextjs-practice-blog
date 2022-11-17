198108a1918af6f4a6f77f440b169aaa39f07d04
QdotD <quinton.digital@protonmail.com>
2022-11-17 09:59:25 -0500

    initial commit for nextJS tutorial -- created nextJS app, added page navigation, assets, layout, and some CSS

---
+++ pages/posts/1.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>#1</title>
+        <link rel="icon" href="/favicon2.ico" />
+      </Head>
+      <h1>Donuts Yum Yum</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
