d80d0875134bc0c01f3bdbfe14acb51181273189
QdotD <quinton.digital@protonmail.com>
2022-11-17 10:44:29 -0500

    finished CSS tutorial -- global CSS and CSS Modules

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import '../styles/global.css';
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (<>
+        <h1>test</h1>
+    <Component {...pageProps}/>
+    <h1>test2</h1>
+    </>
+    );
 }
