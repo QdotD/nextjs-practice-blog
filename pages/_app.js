c833aed564eee5d77f85c2cd938da6df7ad694f5
QdotD <quinton.digital@protonmail.com>
2022-12-01 12:37:15 -0500

    added dynamic routing and data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
     return (<>
-        <h1>test</h1>
+        {/* <h1>test</h1> */}
     <Component {...pageProps}/>
-    <h1>test2</h1>
+    {/* <h1>test2</h1> */}
     </>
     );
 }
--- pages/posts/1.js
+++ pages/posts/1.js
@@ -1,19 +1,22 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Layout from '../../components/layout';
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import Layout from '../../components/layout';
 
 
-export default function FirstPost() {
-  return (
-    <Layout>
-      <Head>
-        <title>#1</title>
-        <link rel="icon" href="/favicon2.ico" />
-      </Head>
-      <h1>Donuts Yum Yum</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </Layout>
-  );
-}
+// export default function FirstPost() {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>#1</title>
+//         <link rel="icon" href="/favicon2.ico" />
+//       </Head>
+//       <h1>Donuts Yum Yum</h1>
+//       <h2>
+//         <Link href="/">Back to home</Link>
+//       </h2>
+//     </Layout>
+//   );
+// }
+
+
+// PAGE NO LONGER NEEDED ****************************
